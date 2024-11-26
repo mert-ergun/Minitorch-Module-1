d6520a1b3d0e72b476fd8f9b39c69e5d38dba70f
mertergunjt <mertergun@jotform.com>
2024-11-27 00:45:36 +0300

    Implement central difference approximation in autodiff.py

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,8 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    slope = (f(*vals[:arg], vals[arg] + epsilon, *vals[arg + 1 :]) - f(*vals)) / epsilon
+    return slope
 
 
 variable_count = 1

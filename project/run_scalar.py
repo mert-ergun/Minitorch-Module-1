b46c494642303f14fb4b48e78aa78e38efeaa5d7
mertergunjt <mertergun@jotform.com>
2024-11-27 15:20:50 +0300

    Update training parameters in ScalarTrain for improved performance

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -109,7 +109,7 @@ class ScalarTrain:
 
 if __name__ == "__main__":
     PTS = 50
-    HIDDEN = 2
-    RATE = 0.5
+    HIDDEN = 10
+    RATE = 0.03
     data = minitorch.datasets["Simple"](PTS)
     ScalarTrain(HIDDEN).train(data, RATE)
